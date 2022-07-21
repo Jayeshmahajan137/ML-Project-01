eae767b714f3ad871fd091244440941515633eea
Jayeshmahajan137 <jayesh.mhjn137@gmail.com>
2022-07-20 21:07:43 -0700

    simple flask app created

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+
+def index():
+    return " Starting project in machine learning, simple project for practice"
+
+   
+if __name__== "__main__":
+    app.run(debug=True)
+    
+
+   
