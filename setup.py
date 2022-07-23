b3c32516afabe3f694062149b5229b03dfc54a63
Jayeshmahajan137 <jayesh.mhjn137@gmail.com>
2022-07-22 19:37:57 -0700

    file updated

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import  setup
+from setuptools import  setup, find_packages
 from typing import List
 
 def get_requirements_list()->List[str]:
@@ -11,11 +11,11 @@ def get_requirements_list()->List[str]:
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         return requirement_file.readlines().remove(" -e .")
+
 PROJECT_NAME="housing_predictor"
 VERSION= "0.0.3"
 AUTHOR="Jayesh"
 DESCRIPTION= "First major project of machine learning "
-PACKAGES=["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 setup(
@@ -23,7 +23,7 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESCRIPTION,
-packages=PACKAGES,
+packages=find_packages(),
 install_requires=get_requirements_list()
 
 )
