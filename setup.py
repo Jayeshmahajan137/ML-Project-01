382c7420f16a96ecb88c54d9e96cbac9637d69e4
Jayeshmahajan137 <jayesh.mhjn137@gmail.com>
2022-07-24 20:40:31 -0700

    changes done in files

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
+from setuptools import setup
+
 from setuptools import setup,find_packages
 from typing import List
 
-def get_requirements_list()->List[str]:
-    with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines().remove("-e .")
-   
-    
-# Declaring variables for setup function
-
-PROJECT_NAME="housing_predictor"
-AUTHOR= "Jayesh"
-VERSION="0.0.4"
-DESCRIPTION="This is first major project in machine learning"
+#Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.2"
+AUTHOR="Jayesh"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
 
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 
+def get_requirements_list()->List[str]:
+    """
+   
+    Description: This function is going to return list of requirement 
+    mention in requirements.txt file
+    return This function is going to return a list which contain name 
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines().remove('-e .')
+
+
 setup(
-project_name=PROJECT_NAME,
-author=AUTHOR,
+name=PROJECT_NAME,
 version=VERSION,
-description=DESCRIPTION,
-packages=find_packages(),
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(), 
 install_requires=get_requirements_list()
 )
