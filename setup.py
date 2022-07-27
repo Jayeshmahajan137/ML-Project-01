4c0a101f77f21dc596e4fd0eb088cb42492fdec5
Jayeshmahajan137 <jayesh.mhjn137@gmail.com>
2022-07-26 20:46:29 -0700

    files updatated

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-from setuptools import setup
-
 from setuptools import setup,find_packages
 from typing import List
 
@@ -21,7 +19,7 @@ def get_requirements_list()->List[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines().remove('-e .')
+        return requirement_file.readlines()
 
 
 setup(
